316ed824082626ef7f853ef5cc34a244718a52be
App Academy Student <appacademy@Apps-Mac-mini-6.local>
2015-10-14 12:24:42 -0400

    create users table

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UrlShortener::Application.config.secret_key_base = '4f4d8e9b3ff27023e377ca4dc222e85affaa1058fb9eb694d016466ef58231d63d414a1878854d49f5456580be96f39db492922774fb62120b166bad1591dfd6'
